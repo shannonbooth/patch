7538d7fa588df8e9e67a41c602e9c61540f8aad0
Shannon Booth <shannon.ml.booth@gmail.com>
2023-01-03 16:41:08 +1300

    patch: fix informational message on missing file to patch
    Our output was not fully aligned with how GNU patch tells the user that
    the file to be patched contained in the patch file cannot be found. Fix
    that, and add a test for this case.

--- tests/test_pty.cpp
+++ tests/test_pty.cpp
@@ -34,3 +34,37 @@ Reversed (or previously applied) patch detected!  Assume -R? [n] )");
     EXPECT_FILE_EQ("a", "1\n2\n3\n");
     EXPECT_EQ(term.return_code(), 0);
 }
+
+PATCH_TEST(pty_file_not_found)
+{
+    {
+        Patch::File file("file", std::ios_base::out);
+        file << "1\n2\n3\n";
+        file.close();
+    }
+
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+
+        file << R"(--- a	2023-01-03 11:23:35.634966282 +1300
++++ b	2023-01-03 11:23:41.598960625 +1300
+@@ -1,3 +1,3 @@
+ 1
+-2
++b
+ 3
+)";
+        file.close();
+    }
+
+    PtySpawn term(patch_path, { patch_path, "-i", "diff.patch", nullptr }, "file\n");
+    EXPECT_EQ(term.output(), R"(can't find file to patch at input line 3
+Perhaps you should have used the -p or --strip option?
+The text leading up to this was:
+--------------------------
+|--- a	2023-01-03 11:23:35.634966282 +1300
+|+++ b	2023-01-03 11:23:41.598960625 +1300
+--------------------------
+File to patch: patching file file
+)");
+}

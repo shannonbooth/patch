a7cc832f92a7ebd86e73f685efc158f4d4274000
Shannon Booth <shannon.ml.booth@gmail.com>
2023-01-18 20:00:26 +1300

    tests: port test_defines to patch test

--- tests/test_defines.cpp
+++ tests/test_defines.cpp
@@ -1,55 +1,50 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// Copyright 2022 Shannon Booth <shannon.ml.booth@gmail.com>
+// Copyright 2022-2023 Shannon Booth <shannon.ml.booth@gmail.com>
 
-#include <patch/applier.h>
-#include <patch/parser.h>
-#include <patch/patch.h>
+#include <patch/file.h>
+#include <patch/process.h>
 #include <patch/test.h>
-#include <sstream>
 
-TEST(defines_add_one_line)
+PATCH_TEST(defines_add_one_line)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
---- file.cpp	2022-01-30 13:57:31.173528027 +1300
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << R"(--- file.cpp	2022-01-30 13:57:31.173528027 +1300
 +++ file.cpp	2022-01-30 13:57:36.321216497 +1300
 @@ -1,3 +1,4 @@
  int main()
  {
 +    return 1;
  }
-)");
+)";
+    }
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(int main()
+    {
+        Patch::File file("file.cpp", std::ios_base::out);
+        file << R"(int main()
 {
 }
-)");
+)";
+    }
 
-    std::string expected_output(R"(int main()
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--ifdef", "TEST_PATCH", nullptr });
+    EXPECT_EQ(process.stdout_data(), "patching file file.cpp\n");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 0);
+    EXPECT_FILE_EQ("file.cpp", R"(int main()
 {
 #ifdef TEST_PATCH
     return 1;
 #endif
 }
 )");
-
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::Options options;
-    options.define_macro = "TEST_PATCH";
-
-    Patch::Result result = Patch::apply_patch(output, reject_writer, input_file, patch, options);
-    EXPECT_EQ(result.failed_hunks, 0);
-    EXPECT_FALSE(result.was_skipped);
-    EXPECT_EQ(output.read_all_as_string(), expected_output);
-    EXPECT_EQ(reject.read_all_as_string(), "");
 }
 
-TEST(defines_remove_one_line)
+PATCH_TEST(defines_remove_one_line)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << R"(
 --- file.cpp	2022-01-30 13:57:31.173528027 +1300
 +++ file.cpp	2022-01-30 13:57:36.321216497 +1300
 @@ -1,4 +1,3 @@
@@ -57,40 +52,36 @@ TEST(defines_remove_one_line)
  {
 -    return 0;
  }
-)");
+)";
+    }
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(int main()
+    {
+        Patch::File file("file.cpp", std::ios_base::out);
+        file << R"(int main()
 {
     return 0;
 }
-)");
+)";
+    }
 
-    std::string expected_output(R"(int main()
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--ifdef", "TEST_PATCH", nullptr });
+    EXPECT_EQ(process.stdout_data(), "patching file file.cpp\n");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 0);
+    EXPECT_FILE_EQ("file.cpp", R"(int main()
 {
 #ifndef TEST_PATCH
     return 0;
 #endif
 }
 )");
-
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::Options options;
-    options.define_macro = "TEST_PATCH";
-
-    Patch::Result result = Patch::apply_patch(output, reject_writer, input_file, patch, options);
-    EXPECT_EQ(result.failed_hunks, 0);
-    EXPECT_FALSE(result.was_skipped);
-    EXPECT_EQ(output.read_all_as_string(), expected_output);
-    EXPECT_EQ(reject.read_all_as_string(), "");
 }
 
-TEST(defines_mix)
+PATCH_TEST(defines_mix)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << R"(
 --- file.cpp	2022-01-30 13:57:31.173528027 +1300
 +++ file.cpp	2022-01-30 13:57:36.321216497 +1300
 @@ -1,4 +1,4 @@
@@ -99,15 +90,24 @@ TEST(defines_mix)
 -    return 0;
 +    return 1;
  }
-)");
+)";
+    }
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(int main()
+    {
+        Patch::File file("file.cpp", std::ios_base::out);
+        file <<
+            R"(int main()
 {
     return 0;
 }
-)");
+)";
+    }
 
-    std::string expected_output(R"(int main()
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--ifdef", "TEST_PATCH", nullptr });
+    EXPECT_EQ(process.stdout_data(), "patching file file.cpp\n");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 0);
+    EXPECT_FILE_EQ("file.cpp", R"(int main()
 {
 #ifndef TEST_PATCH
     return 0;
@@ -116,25 +116,13 @@ TEST(defines_mix)
 #endif
 }
 )");
-
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::Options options;
-    options.define_macro = "TEST_PATCH";
-
-    Patch::Result result = Patch::apply_patch(output, reject_writer, input_file, patch, options);
-    EXPECT_EQ(result.failed_hunks, 0);
-    EXPECT_FALSE(result.was_skipped);
-    EXPECT_EQ(output.read_all_as_string(), expected_output);
-    EXPECT_EQ(reject.read_all_as_string(), "");
 }
 
-TEST(defines_changes_at_end_of_file)
+PATCH_TEST(defines_changes_at_end_of_file)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << R"(
 --- main.cpp	2022-02-20 19:38:33.685996435 +1300
 +++ main.cpp	2022-02-20 19:38:48.069436313 +1300
 @@ -1,4 +1,4 @@
@@ -144,15 +132,23 @@ TEST(defines_changes_at_end_of_file)
 -}
 +    return 1;
 +} // some change to this last line.
-)");
+)";
+    }
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(int main()
+    {
+        Patch::File file("main.cpp", std::ios_base::out);
+        file << R"(int main()
 {
     return 0;
 }
-)");
+)";
+    }
 
-    std::string expected_output(R"(int main()
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--ifdef", "TEST_PATCH", nullptr });
+    EXPECT_EQ(process.stdout_data(), "patching file main.cpp\n");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 0);
+    EXPECT_FILE_EQ("main.cpp", R"(int main()
 {
 #ifndef TEST_PATCH
     return 0;
@@ -162,25 +158,13 @@ TEST(defines_changes_at_end_of_file)
 } // some change to this last line.
 #endif
 )");
-
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::Options options;
-    options.define_macro = "TEST_PATCH";
-
-    Patch::Result result = Patch::apply_patch(output, reject_writer, input_file, patch, options);
-    EXPECT_EQ(result.failed_hunks, 0);
-    EXPECT_FALSE(result.was_skipped);
-    EXPECT_EQ(output.read_all_as_string(), expected_output);
-    EXPECT_EQ(reject.read_all_as_string(), "");
 }
 
-TEST(defines_more_complex_patch)
+PATCH_TEST(defines_more_complex_patch)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << R"(
 --- add.cpp	2022-02-26 13:15:21.624635744 +1300
 +++ sub.cpp	2022-02-26 13:15:07.396899118 +1300
 @@ -1,17 +1,17 @@
@@ -206,9 +190,12 @@ TEST(defines_more_complex_patch)
 +	// Some comment
  	return x + y;
  }
-)");
+)";
+    }
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(int add(int a, int b)
+    {
+        Patch::File file("add.cpp", std::ios_base::out);
+        file << R"(int add(int a, int b)
 {
 	return a + b;
 }
@@ -225,9 +212,14 @@ int main()
 	int y = subtract(3, 4);
 	return x + y;
 }
-)");
+)";
+    }
 
-    std::string expected_output(R"(#ifndef MY_PATCH
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--ifdef", "MY_PATCH", nullptr });
+    EXPECT_EQ(process.stdout_data(), "patching file add.cpp\n");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 0);
+    EXPECT_FILE_EQ("add.cpp", R"(#ifndef MY_PATCH
 int add(int a, int b)
 #else
 int subtract(int a, int b)
@@ -266,18 +258,4 @@ int main()
 	return x + y;
 }
 )");
-
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::Options options;
-    options.define_macro = "MY_PATCH";
-
-    Patch::Result result = Patch::apply_patch(output, reject_writer, input_file, patch, options);
-    EXPECT_EQ(result.failed_hunks, 0);
-    EXPECT_FALSE(result.was_skipped);
-    EXPECT_EQ(output.read_all_as_string(), expected_output);
-    EXPECT_EQ(reject.read_all_as_string(), "");
 }

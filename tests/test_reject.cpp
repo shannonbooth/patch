8ab15666341b48cbe51c6007c412ccadc643a715
Shannon Booth <shannon.ml.booth@gmail.com>
2023-01-20 21:25:58 +1300

    tests: port test_reject to patch test

--- tests/test_reject.cpp
+++ tests/test_reject.cpp
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// Copyright 2022 Shannon Booth <shannon.ml.booth@gmail.com>
+// Copyright 2022-2023 Shannon Booth <shannon.ml.booth@gmail.com>
 
-#include <patch/applier.h>
-#include <patch/parser.h>
-#include <patch/patch.h>
+#include <patch/process.h>
 #include <patch/test.h>
-#include <sstream>
 
-TEST(reject_unified_remove_line_no_offset)
+PATCH_TEST(reject_unified_remove_line_no_offset)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
---- 1	2022-04-24 12:58:33.100280281 +1200
+    const std::string patch = R"(--- 1	2022-04-24 12:58:33.100280281 +1200
 +++ 2	2022-04-24 12:58:31.560276720 +1200
 @@ -2,7 +2,6 @@
  2
@@ -20,9 +16,16 @@ TEST(reject_unified_remove_line_no_offset)
  6
  7
  8
-)");
+)";
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(1
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << patch;
+    }
+
+    {
+        Patch::File file("1", std::ios_base::out);
+        file << R"(1
 2
 3
 5
@@ -32,35 +35,23 @@ TEST(reject_unified_remove_line_no_offset)
 8
 9
 10
+)";
+    }
+
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--force", nullptr });
+    EXPECT_EQ(process.stdout_data(), R"(patching file 1
+Hunk #1 FAILED at 2.
+1 out of 1 hunk FAILED -- saving rejects to file 1.rej
 )");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 1);
 
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::apply_patch(output, reject_writer, input_file, patch);
-    EXPECT_EQ(output.read_all_as_string(), input_file.read_all_as_string());
-
-    EXPECT_EQ(reject.read_all_as_string(),
-        R"(--- 1	2022-04-24 12:58:33.100280281 +1200
-+++ 2	2022-04-24 12:58:31.560276720 +1200
-@@ -2,7 +2,6 @@
- 2
- 3
- 4
--5
- 6
- 7
- 8
-)");
+    EXPECT_FILE_EQ("1.rej", patch);
 }
 
-TEST(reject_context_remove_line)
+PATCH_TEST(reject_context_remove_line)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
-*** a.cpp	2022-04-25 10:47:18.388073392 +1200
+    const std::string patch = R"(*** a.cpp	2022-04-25 10:47:18.388073392 +1200
 --- b.cpp	2022-04-25 10:04:06.012170915 +1200
 ***************
 *** 1,4 ****
@@ -69,41 +60,38 @@ TEST(reject_context_remove_line)
 - 	return 0;
   }
 --- 1,3 ----
-)");
+)";
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(int main()
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << patch;
+    }
+
+    {
+        Patch::File file("a.cpp", std::ios_base::out);
+        file << R"(int main()
 {
 /	return 0;
 }
+)";
+    }
+
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--force", nullptr });
+    EXPECT_EQ(process.stdout_data(), R"(patching file a.cpp
+Hunk #1 FAILED at 1.
+1 out of 1 hunk FAILED -- saving rejects to file a.cpp.rej
 )");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 1);
 
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::Options options;
-    options.force = true;
-    Patch::apply_patch(output, reject_writer, input_file, patch, options);
-
-    EXPECT_EQ(output.read_all_as_string(), input_file.read_all_as_string());
-
-    EXPECT_EQ(reject.read_all_as_string(),
-        R"(*** a.cpp	2022-04-25 10:47:18.388073392 +1200
---- b.cpp	2022-04-25 10:04:06.012170915 +1200
-***************
-*** 1,4 ****
-  int main()
-  {
-- 	return 0;
-  }
---- 1,3 ----
-)");
+    EXPECT_FILE_EQ("a.cpp.rej", patch);
 }
 
-TEST(reject_offset_in_previous_hunk_applies_to_reject)
+PATCH_TEST(reject_offset_in_previous_hunk_applies_to_reject)
 {
-    Patch::File patch_file = Patch::File::create_temporary_with_content(R"(
+    {
+        Patch::File file("diff.patch", std::ios_base::out);
+        file << R"(
 --- main.cpp	2022-06-07 20:08:07.722685716 +1200
 +++ second_main.cpp	2022-06-07 20:08:22.863130397 +1200
 @@ -1,5 +1,6 @@
@@ -120,9 +108,12 @@ TEST(reject_offset_in_previous_hunk_applies_to_reject)
 -	return 0;
 +	return 1;
  }
-)");
+)";
+    }
 
-    Patch::File input_file = Patch::File::create_temporary_with_content(R"(// newly added line
+    {
+        Patch::File file("main.cpp", std::ios_base::out);
+        file << R"(// newly added line
 // ... and another
 int main()
 {
@@ -137,34 +128,19 @@ int one_more() // comments to
 { // cause
 	return 0;
 } // reject
+)";
+    }
+
+    Process process(patch_path, { patch_path, "-i", "diff.patch", "--force", nullptr });
+    EXPECT_EQ(process.stdout_data(), R"(patching file main.cpp
+Hunk #1 succeeded at 3 (offset 2 lines).
+Hunk #2 FAILED at 11.
+1 out of 2 hunks FAILED -- saving rejects to file main.cpp.rej
 )");
+    EXPECT_EQ(process.stderr_data(), "");
+    EXPECT_EQ(process.return_code(), 1);
 
-    auto patch = Patch::parse_patch(patch_file);
-    Patch::File output = Patch::File::create_temporary();
-    Patch::File reject = Patch::File::create_temporary();
-    Patch::RejectWriter reject_writer(patch, reject);
-
-    Patch::apply_patch(output, reject_writer, input_file, patch);
-
-    EXPECT_EQ(output.read_all_as_string(), R"(// newly added line
-// ... and another
-int main()
-{
-	return 0;
-}
-//
-int another()
-{
-	return 1 - 2;
-}
-// some
-int one_more() // comments to
-{ // cause
-	return 0;
-} // reject
-)");
-
-    EXPECT_EQ(reject.read_all_as_string(), R"(--- main.cpp	2022-06-07 20:08:07.722685716 +1200
+    EXPECT_FILE_EQ("main.cpp.rej", R"(--- main.cpp	2022-06-07 20:08:07.722685716 +1200
 +++ second_main.cpp	2022-06-07 20:08:22.863130397 +1200
 @@ -10,5 +11,5 @@
  //
